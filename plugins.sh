a4f289bc4db814197b9e6a3256310369ba93da7c
phantom diorama <phantomdiorama@gmail.com>
2023-12-11 15:07:59 +1100

    added plugin script

---
+++ plugins.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/Raimondi/delimitMate.git
+git clone https://github.com/ajorgensen/vim-markdown-toc.git
+git clone https://github.com/godlygeek/tabular.git
+git clone https://github.com/junegunn/goyo.vim.git
+git clone https://github.com/justinmk/vim-sneak.git
+git clone https://github.com/kana/vim-textobj-user.git
+git clone https://github.com/kblin/vim-fountain.git
+git clone https://github.com/lifepillar/vim-mucomplete.git
+git clone https://github.com/machakann/vim-highlightedyank.git
+git clone https://github.com/mmikeww/autohotkey.vim.git
+git clone https://github.com/preservim/vim-litecorrect.git
+git clone https://github.com/preservim/vim-textobj-quote.git
+git clone https://github.com/romainl/vim-tinyMRU.git
+git clone https://github.com/tommcdo/vim-exchange.git
+git clone https://github.com/tpope/vim-abolish.git
+git clone https://github.com/tpope/vim-commentary.git
