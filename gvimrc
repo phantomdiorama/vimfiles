ddbbbd3eb85d2638a6a9eeb280f2f1a35bf4b740
phantom diorama <phantomdiorama@gmail.com>
2024-09-28 13:18:44 +1000

    plugin script cleanup

--- gvimrc
+++ gvimrc
@@ -2,13 +2,11 @@
 set guioptions-=T               " no toolbar cos useless
 set guioptions-=e               " no toolbar cos useless
 set guioptions-=r               " no scrollbar
-" set guioptions-=m               " no menubar
 
-" set background=light
 colorscheme lucius
 LuciusWhiteHighContrast
 
-highlight SpellBad   guifg=black guibg=#D7AFAF
+highlight SpellBad   guibg=#D7AFAF
 highlight SpecialKey guifg=magenta
 
 if has("gui_win32")
--- plugins.sh
+++ plugins.sh
@@ -6,18 +6,11 @@ mkdir -p ~/.vim/pack/bundle/start
 # download plugins
 cd ~/.vim/pack/bundle/start
 git clone https://github.com/Raimondi/delimitMate.git
-git clone https://github.com/ajorgensen/vim-markdown-toc.git
-git clone https://github.com/godlygeek/tabular.git
-git clone https://github.com/junegunn/goyo.vim.git
 git clone https://github.com/kana/vim-textobj-user.git
 git clone https://github.com/kblin/vim-fountain.git
 git clone https://github.com/lifepillar/vim-mucomplete.git
 git clone https://github.com/machakann/vim-highlightedyank.git
-git clone https://github.com/mmikeww/autohotkey.vim.git
 git clone https://github.com/preservim/vim-litecorrect.git
 git clone https://github.com/preservim/vim-textobj-quote.git
 git clone https://github.com/romainl/vim-tinyMRU.git
-git clone https://github.com/tommcdo/vim-exchange.git
-git clone https://github.com/tpope/vim-abolish.git
 git clone https://github.com/tpope/vim-commentary.git
-git clone https://github.com/tpope/vim-surround.git
